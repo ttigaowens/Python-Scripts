0bb53b39e78fb9cdec16f3770a4624e38c7dac71
ttigaowens <aaron.owens@thetechinfogroup.com>
2014-07-16 13:21:00 -0700

    New file
    added a file

---
+++ killusers.py
@@ -0,0 +1,17 @@
+import os
+
+exempt = ["Public", "Default", ".NET", "All"]
+
+for d in os.listdir('c:\\users\\'):
+        newpath = str("C:\\users\\" + d)
+        try:
+                pos = d.index(" ")
+        except:
+                pos = len(d)
+        if  d[0:pos] not in exempt:
+                #os.system("mkdir " + newpath)
+                os.system("rd " + newpath + " /s /q")
+                print d + " - DELETED"
+                        
+        else:
+                print d
